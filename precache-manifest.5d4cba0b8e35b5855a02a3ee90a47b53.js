669ea5a09dca5fd25e913bb0b5bcacba14e22032
mwoolf87 <michael.j.woolf@gmail.com>
2019-04-05 15:02:02 -0400

    Updates

---
+++ precache-manifest.5d4cba0b8e35b5855a02a3ee90a47b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "fadb15d085650e037d85",
+    "url": "/contactmanager/static/js/main.2fa27a38.chunk.js"
+  },
+  {
+    "revision": "0bafae586329521963ae",
+    "url": "/contactmanager/static/js/2.97416df2.chunk.js"
+  },
+  {
+    "revision": "fadb15d085650e037d85",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "0bafae586329521963ae",
+    "url": "/contactmanager/static/css/2.ef3a6ca1.chunk.css"
+  },
+  {
+    "revision": "2090ffef709108adc68eaec0a1b715d0",
+    "url": "/contactmanager/index.html"
+  }
+];
